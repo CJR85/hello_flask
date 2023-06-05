5a6080cf5a965a2de4e12a8c589b8cf000834487
Calvin Roberts <roberts.calvin85@gmail.com>
2023-06-05 23:54:28 +0100

    Working Flask URL paths and Flask Debugger

--- hello.py
+++ hello.py
@@ -8,5 +8,16 @@ print(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/bye')
+def bye():
+    return "Bye!"
+
+@app.route('/username/<name>')
+def greet(name):
+    return f"Hello {name}!"
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+greet()
